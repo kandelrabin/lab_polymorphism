e2eeead026824c0cbbaa0fae7e02aa070429ffe8
Rabin Kandel <kandelrabin@live.com>
2024-05-14 19:30:51 +0100

    Complete Helicopter class and tests

--- src/test/java/HelicopterTest.java
+++ src/test/java/HelicopterTest.java
@@ -1,2 +1,52 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class HelicopterTest {
+
+    Helicopter helicopter;
+
+    @BeforeEach
+    public void setUp(){
+        helicopter = new Helicopter("Batcopter", 1500.00, "Air", 7, false, "Heathrow");
+    }
+
+    @Test
+    public void canCheckIsInSky(){
+        assertThat(helicopter.isInSky()).isEqualTo(false);
+    }
+
+    @Test
+    public void canTakeOff(){
+        assertThat(helicopter.takeOff()).isEqualTo("Engine is turned off!");
+        helicopter.startEngine();
+        assertThat(helicopter.takeOff()).isEqualTo("I am taking off!");
+        assertThat(helicopter.takeOff()).isEqualTo("Already in the sky");
+    }
+
+    @Test
+    public void canLand(){
+        assertThat(helicopter.land()).isEqualTo("Already landed!");
+        helicopter.startEngine();
+        helicopter.takeOff();
+        assertThat(helicopter.land()).isEqualTo("I am landing!");
+    }
+
+    @Test
+    public void canGetAirport(){
+        assertThat(helicopter.getAirport()).isEqualTo("Heathrow");
+    }
+
+    @Test
+    public void canGetFuelType(){
+        assertThat(helicopter.getFuelType()).isEqualTo("");
+    }
+
+    @Test
+    public void canSetFuelType(){
+        helicopter.setFuelType("Jet Fuel");
+        assertThat(helicopter.getFuelType()).isEqualTo("Jet Fuel");
+    }
+
 }

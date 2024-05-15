48225d7308f8578dc99a00eb537f5f849508dba2
Rabin Kandel <kandelrabin@live.com>
2024-05-15 18:48:00 +0100

    Add VehicleHireShop class and tests

--- src/main/java/Models/VehicleHireShop.java
+++ src/main/java/Models/VehicleHireShop.java
@@ -1,4 +1,60 @@
 package Models;
 
+import Interfaces.Hireable;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class VehicleHireShop {
+
+    private String name;
+    private HashMap<String, Hireable> hiredItems;
+    private double till;
+
+    public VehicleHireShop(String name){
+        this.name = name;
+        this.hiredItems = new HashMap<>();
+        this.till = 0.00;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addItemToHiredItems(String key, Hireable hireable){
+        this.hiredItems.put(key, hireable);
+    }
+
+    public Hireable getItemFromHiredItems(String key){
+        return this.hiredItems.get(key);
+    }
+
+    public void removeItemFromHiredItems(String key){
+        this.hiredItems.remove(key);
+    }
+
+    public int getHiredItemsSize(){
+        return this.hiredItems.size();
+    }
+
+    public double getTill() {
+        return this.till;
+    }
+
+    public void depositToTill(double amount){
+        this.till += amount;
+    }
+
+    public double calculateHiredItemsEarnings(){
+        double total = 0.00;
+        for (Map.Entry<String, Hireable> set : hiredItems.entrySet()){
+            total += set.getValue().calculateCost();
+        }
+        return total;
+    }
+
 }
--- src/test/java/VehicleHireShopTest.java
+++ src/test/java/VehicleHireShopTest.java
@@ -1,2 +1,82 @@
+import Interfaces.Hireable;
+import Models.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class VehicleHireShopTest {
+
+    VehicleHireShop vehicleHireShop;
+    Accessory satNav;
+    Car car;
+    Boat boat;
+    Helicopter helicopter;
+    Train train;
+
+    @BeforeEach
+    public void setUp(){
+        vehicleHireShop = new VehicleHireShop("Enterprise");
+        satNav = new Accessory("SatNav", 100.00, 3);
+        car = new Car("Mercedes", "C-Class", 2020, 500.00, 3);
+        boat = new Boat("River Navigator", "Docklands", 400.00, 1);
+        helicopter = new Helicopter("Bat Copter", "Heathrow", 2000.00, 1);
+        train = new Train("NWR", "London", "Manchester", 5000.00,1);
+    }
+
+    @Test
+    public void canGetName(){
+        assertThat(vehicleHireShop.getName()).isEqualTo("Enterprise");
+    }
+
+    @Test
+    public void canSetName(){
+        vehicleHireShop.setName("Sixt");
+        assertThat(vehicleHireShop.getName()).isEqualTo("Sixt");
+    }
+
+    @Test
+    public void canAddItemToCatalogue(){
+        String key = car.getName()+car.getModel()+car.getYear();
+        vehicleHireShop.addItemToHiredItems(key, car);
+        assertThat(vehicleHireShop.getItemFromHiredItems(key)).isEqualTo(car);
+    }
+
+    @Test
+    public void canGetItemFromCatalogue(){
+        int result = vehicleHireShop.getHiredItemsSize();
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void removeItemFromCatalogue(){
+        vehicleHireShop.addItemToHiredItems("car001", car);
+        vehicleHireShop.addItemToHiredItems("train001", train);
+        assertThat(vehicleHireShop.getHiredItemsSize()).isEqualTo(2);
+
+        vehicleHireShop.removeItemFromHiredItems("train001");
+        assertThat(vehicleHireShop.getHiredItemsSize()).isEqualTo(1);
+    }
+
+    @Test
+    public void canGetTill(){
+        assertThat(vehicleHireShop.getTill()).isEqualTo(0.00);
+    }
+
+    @Test
+    public void canUpdateTill(){
+        vehicleHireShop.depositToTill(1000.00);
+        assertThat(vehicleHireShop.getTill()).isEqualTo(1000.00);
+    }
+
+    @Test
+    public void canCalculateHiredItemsEarnings(){
+        vehicleHireShop.addItemToHiredItems("car001", car);
+        vehicleHireShop.addItemToHiredItems("train001", train);
+        double expected = car.calculateCost() + train.calculateCost();
+        double result = vehicleHireShop.calculateHiredItemsEarnings();
+        assertThat(result).isEqualTo(expected);
+    }
+
+
 }
